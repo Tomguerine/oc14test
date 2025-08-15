a7db74c71d751cc4bff48437024dcbb65473297e
Guerineau Tom <124735919+Tomguerine@users.noreply.github.com>
2025-08-15 18:27:59 +0200

    Add employee creation form with Radix components

---
+++ hrnet-react/src/components/DatePicker.tsx
@@ -0,0 +1,15 @@
+import { InputHTMLAttributes } from 'react'
+
+type DatePickerProps = InputHTMLAttributes<HTMLInputElement> & {
+  label: string
+}
+
+export default function DatePicker({ label, id, ...props }: DatePickerProps) {
+  return (
+    <div>
+      <label htmlFor={id}>{label}</label>
+      <input type="date" id={id} {...props} />
+    </div>
+  )
+}
+
