45b0762046ff80cb0a00be538994d325880255d6
unknown <tomguerineaupro@gmail.com>
2025-08-21 19:04:13 +0200

    fix

--- modal-popup/dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Modal = void 0;
-const Modal_1 = __importDefault(require("./Modal"));
-exports.Modal = Modal_1.default;
-exports.default = Modal_1.default;
