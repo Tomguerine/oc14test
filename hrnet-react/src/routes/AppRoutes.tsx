fb1f1acebe90b821350e44ac1fb6473e42645a06
Guerineau Tom <124735919+Tomguerine@users.noreply.github.com>
2025-08-21 16:40:45 +0200

    Use lazy loading for routes

--- hrnet-react/src/routes/AppRoutes.tsx
+++ hrnet-react/src/routes/AppRoutes.tsx
@@ -1,12 +1,16 @@
 import { Routes, Route } from 'react-router-dom'
-import CreateEmployee from '../pages/CreateEmployee'
-import CurrentEmployees from '../pages/CurrentEmployees'
+import { Suspense, lazy } from 'react'
+
+const CreateEmployee = lazy(() => import('../pages/CreateEmployee'))
+const CurrentEmployees = lazy(() => import('../pages/CurrentEmployees'))
 
 export default function AppRoutes() {
   return (
-    <Routes>
-      <Route path="/" element={<CreateEmployee />} />
-      <Route path="/employees" element={<CurrentEmployees />} />
-    </Routes>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path="/" element={<CreateEmployee />} />
+        <Route path="/employees" element={<CurrentEmployees />} />
+      </Routes>
+    </Suspense>
   )
 }
